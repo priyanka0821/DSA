10b8a3ee2c202ca360277b653885ac874f6417bc
priyanka0821 <cse22184@glbitm.ac.in>
2025-09-04 19:29:02 +0530

    Time: 4 ms (83.67%) | Memory: 69.2 MB (10.82%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,22 +9,44 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        Stack<Integer> st=new Stack<>();
-        ListNode temp=head;
-        while(temp!=null){
-            st.push(temp.val);
-            temp=temp.next;
+    public static ListNode reverse(ListNode head){
+        ListNode curr=head, prev=null, next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
         }
-        temp=head;
-        while(temp!=null){
-            if(temp.val==st.peek()){
-                temp=temp.next;
-                st.pop();
-            }else{
-                return false;
-            }
+        return prev;
+    }
+
+    public static boolean compareLL(ListNode head1, ListNode head2){
+        ListNode temp1=head1, temp2=head2;
+        while(temp2!=null){
+            if(temp1.val==temp2.val){
+                temp1=temp1.next;
+                  temp2=temp2.next;
+            }else return false;
         }
         return true;
+    }
+    public boolean isPalindrome(ListNode head) {
+       //find middle of ll
+       ListNode slow=head, fast=head;
+       while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+       }
+
+       //reverse second half
+       ListNode head2=reverse(slow);
+
+       //compare both half
+     boolean isPal =compareLL(head,head2);
+     //again reverse second half;
+     reverse(slow);
+     return isPal;
+
+
     }
 }
